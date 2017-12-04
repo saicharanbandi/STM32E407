ff3eb75a36df637e163c434b6904b507cc931ec7
saicharanbandi <charan.sai131@gmail.com>
2017-12-04 15:18:55 +0100

    Manch_Enc: This version is for forum question; I think I found one more way which could work!

--- Test_Project/Src/stm32f4xx_it.c
+++ Test_Project/Src/stm32f4xx_it.c
@@ -1,55 +1,54 @@
 /**
-******************************************************************************
-* @file    stm32f4xx_it.c
-* @brief   Interrupt Service Routines.
-******************************************************************************
-*
-* COPYRIGHT(c) 2017 STMicroelectronics
-*
-* Redistribution and use in source and binary forms, with or without modification,
-* are permitted provided that the following conditions are met:
-*   1. Redistributions of source code must retain the above copyright notice,
-*      this list of conditions and the following disclaimer.
-*   2. Redistributions in binary form must reproduce the above copyright notice,
-*      this list of conditions and the following disclaimer in the documentation
-*      and/or other materials provided with the distribution.
-*   3. Neither the name of STMicroelectronics nor the names of its contributors
-*      may be used to endorse or promote products derived from this software
-*      without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-******************************************************************************
-*/
+  ******************************************************************************
+  * @file    stm32f4xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2017 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx.h"
 #include "stm32f4xx_it.h"
 
 /* USER CODE BEGIN 0 */
-/* Variable declaration */
-
-/* temp[] vector */
-extern int i;
-extern uint16_t tmp[28];
-extern uint16_t tmp2[28];
-/* uwDiffCapture will be initialized in Main() */
-volatile extern uint16_t uwDiffCapture;
-/* uIRQ_Temp variable will be initialized in Main() */
-volatile extern unsigned char uIRQ_Temp;
-/* uPulse_Width_2T variable will be initialized here */
-extern unsigned char uPulse_Width_2T;
-/* start_capture variable to designate that the hardware_trigger occurred */
+/* Use start_capture variable to suggest that hardware_trigger occured */
 extern unsigned char start_capture;
+
+/* temp vector */
+volatile uint16_t tmp[2] = {0, 0};
+
+/* uwDiffCapture will be initialized in main () */
+volatile uint16_t uwDiffCapture = 0;
+/* uIRQ_Temp will be initialized in main() */
+volatile extern unsigned char uIRQ_Temp;
+volatile extern unsigned char uPulse_width_2T;
+volatile extern unsigned char uPulse_width_T;
+
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
@@ -60,8 +59,8 @@ extern TIM_HandleTypeDef htim4;
 /******************************************************************************/
 
 /**
- * @brief This function handles Non maskable interrupt.
- */
+* @brief This function handles Non maskable interrupt.
+*/
 void NMI_Handler(void)
 {
   /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
@@ -73,72 +72,72 @@ void NMI_Handler(void)
 }
 
 /**
- * @brief This function handles Hard fault interrupt.
- */
+* @brief This function handles Hard fault interrupt.
+*/
 void HardFault_Handler(void)
 {
   /* USER CODE BEGIN HardFault_IRQn 0 */
 
   /* USER CODE END HardFault_IRQn 0 */
   while (1)
-    {
-    }
+  {
+  }
   /* USER CODE BEGIN HardFault_IRQn 1 */
 
   /* USER CODE END HardFault_IRQn 1 */
 }
 
 /**
- * @brief This function handles Memory management fault.
- */
+* @brief This function handles Memory management fault.
+*/
 void MemManage_Handler(void)
 {
   /* USER CODE BEGIN MemoryManagement_IRQn 0 */
 
   /* USER CODE END MemoryManagement_IRQn 0 */
   while (1)
-    {
-    }
+  {
+  }
   /* USER CODE BEGIN MemoryManagement_IRQn 1 */
 
   /* USER CODE END MemoryManagement_IRQn 1 */
 }
 
 /**
- * @brief This function handles Pre-fetch fault, memory access fault.
- */
+* @brief This function handles Pre-fetch fault, memory access fault.
+*/
 void BusFault_Handler(void)
 {
   /* USER CODE BEGIN BusFault_IRQn 0 */
 
   /* USER CODE END BusFault_IRQn 0 */
   while (1)
-    {
-    }
+  {
+  }
   /* USER CODE BEGIN BusFault_IRQn 1 */
 
   /* USER CODE END BusFault_IRQn 1 */
 }
 
 /**
- * @brief This function handles Undefined instruction or illegal state.
- */
+* @brief This function handles Undefined instruction or illegal state.
+*/
 void UsageFault_Handler(void)
 {
   /* USER CODE BEGIN UsageFault_IRQn 0 */
 
   /* USER CODE END UsageFault_IRQn 0 */
   while (1)
-    {
-    }
+  {
+  }
   /* USER CODE BEGIN UsageFault_IRQn 1 */
 
   /* USER CODE END UsageFault_IRQn 1 */
 }
 
 /**
- * @brief This function handles System service call via SWI instruction.
- */
+* @brief This function handles System service call via SWI instruction.
+*/
 void SVC_Handler(void)
 {
   /* USER CODE BEGIN SVCall_IRQn 0 */
@@ -150,8 +149,8 @@ void SVC_Handler(void)
 }
 
 /**
- * @brief This function handles Debug monitor.
- */
+* @brief This function handles Debug monitor.
+*/
 void DebugMon_Handler(void)
 {
   /* USER CODE BEGIN DebugMonitor_IRQn 0 */
@@ -163,8 +162,8 @@ void DebugMon_Handler(void)
 }
 
 /**
- * @brief This function handles Pendable request for system service.
- */
+* @brief This function handles Pendable request for system service.
+*/
 void PendSV_Handler(void)
 {
   /* USER CODE BEGIN PendSV_IRQn 0 */
@@ -176,8 +175,8 @@ void PendSV_Handler(void)
 }
 
 /**
- * @brief This function handles System tick timer.
- */
+* @brief This function handles System tick timer.
+*/
 void SysTick_Handler(void)
 {
   /* USER CODE BEGIN SysTick_IRQn 0 */
@@ -198,46 +197,81 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
- * @brief This function handles TIM4 global interrupt.
- */
+* @brief This function handles TIM4 global interrupt.
+**/
+
 void TIM4_IRQHandler(void)
 {
   /* USER CODE BEGIN TIM4_IRQn 0 */
-  /* uIRQ_Temp = 0; */
+
   /* USER CODE END TIM4_IRQn 0 */
   HAL_TIM_IRQHandler(&htim4);
-
   /* USER CODE BEGIN TIM4_IRQn 1 */
+  uPulse_width_2T = 0;
+  uPulse_width_T = 0;
+  uIRQ_Temp = 0;
   tmp[1] = HAL_TIM_ReadCapturedValue(&htim4, TIM_CHANNEL_3);
   
   if(tmp[1] > tmp[0])
     {
-      uwDiffCapture = (tmp[1]-tmp[0]);
+      uwDiffCapture = tmp[1] - tmp[0];
     }
-  else /* tmp[1] <= tmp[0] */
+  else /* if tmp[1] <= tmp[0]*/
     {
-      uwDiffCapture = (0xFFFF-tmp[1])+tmp[0];
+      uwDiffCapture = (0xFFFF - tmp[1]) + tmp[0];
     }
+
   /* Checking for pulse of 2T width */
-  if((uwDiffCapture > 12600) && (uwDiffCapture < 12900))
+  if((uwDiffCapture > 6300) && (uwDiffCapture < 6500))
     {
-       
+      uPulse_width_T = 1;
+      HAL_GPIO_WritePin(LED_Blue_GPIO_Port, LED_Blue_Pin, GPIO_PIN_SET);
     }
-  
+  else if((uwDiffCapture > 12700) && (uwDiffCapture < 12900))
+       {
+  	 uPulse_width_2T = 1;
+  	 HAL_GPIO_WritePin(LED_Yellow_GPIO_Port, LED_Yellow_Pin, GPIO_PIN_SET);
+       }
+
+
+
   /* Left shifting the vector */
   tmp[0] = tmp[1];
-  /* Updating this variable tells that interrupt has occurred */
-  uIRQ_Temp = 1;
+
+  /* if((uwDiffCapture > 12700) && (uwDiffCapture < 12900)) */
+  /*   { */
+  /*     variable1 = 1; */
+  /*   } */
+
+  /* /\* Execute this code only if the variable1 = 1 *\/ */
+  /*     /\* Read state and write to Current bit; *\/ */
+  /*     if ((uwDiffCapture > 6300) && (uwDiffCapture < 6500)) */
+  /* 	{ */
+  /* 	  /\* Here update the receive buffer index *\/ */
+  /* 	  if ((uwDiffCapture > 6300) && (uwDiffCapture < 6500)) */
+  /* 	    { */
+  /* 	      tmp[i] = Current_bit; */
+  /* 	    } */
+  /* 	  else { */
+  /* 	    Error; */
+  /* 	  } */
+  /* 	} */
+  /*     else if((uwDiffCapture > 12700) && (uwDiffCapture > 12900)) */
+  /* 	{ */
+  /* 	  tmp[i] = ~ Current_bit; */
+  /* 	} */
+  /*     i++; */
+  /*     return i; */
+  /*   } */
+    
   
- 
-  
-  
+
   /* USER CODE END TIM4_IRQn 1 */
 }
 
 /**
- * @brief This function handles EXTI line[15:10] interrupts.
- */
+* @brief This function handles EXTI line[15:10] interrupts.
+*/
 void EXTI15_10_IRQHandler(void)
 {
   /* USER CODE BEGIN EXTI15_10_IRQn 0 */
